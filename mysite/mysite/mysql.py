91dd0b37d030083c3d29279391c5b923bed0d1e8
LarryBrick <yananback@gmail.com>
2013-09-16 00:47:56 +0800

    my half

---
+++ mysite/mysite/mysql.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render_to_response
+import MySQLdb
+
+def book_list(request):
+    db = MySQLdb.connect(user='me',db='mydb',passwd='secret',host='localhost')
+    cursor = db.cursor()
+    cursor.execute('SELECT name FROM books ORDER BY name')
+    names = [row[0] for row in cursor.fetchall()]
+    db.close()
+    return render_to_response('book_list.html',{'names':names})
